e84c192aba62dca5c4f9e2eea2bb1e6c72d6f664
Mahdi Yari <m.yary15@gmail.com>
2018-07-23 00:48:27 +0430

    add blog pages to the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,24 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes  } from '@angular/router';
-import { AppComponent }   from './app.component';
-import { HomeComponent }   from './home/home.component';
-import { NotfoundComponent }   from './notfound/notfound.component';
-import { AboutComponent }   from './about/about.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
+import { NgModule }             from '@angular/core'
+import { RouterModule, Routes, UrlSegment  } from '@angular/router'
+import { HomeComponent }   from './home/home.component'
+import { NotfoundComponent }   from './notfound/notfound.component'
+import { AboutComponent }   from './about/about.component'
+import { DashboardComponent }   from './dashboard/dashboard.component'
+import { BlogComponent } from './blog/blog.component';
+
+// A regex for blog page
+// it will accept values like @mahdiyari, @mahdi-yari, @mahdi.yari
+export function blogPath(url: UrlSegment[]) {
+  return url.length === 1 && url[0].path.match(/^[\@]+[a-zA-Z0-9\-\.]+$/) ? ({consumed: url}) : null;
+}
 
 const routes: Routes = [
   { path: '' , component: HomeComponent },
   { path: 'about' , component: AboutComponent },
   { path: 'dashboard' , component: DashboardComponent },
+  { matcher: blogPath, component: BlogComponent },
   { path: '**' , component: NotfoundComponent }
-];
+]
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
